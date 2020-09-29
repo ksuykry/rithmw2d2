d93f8915e2ec18f2e9b7a3be254f036bc532ddde
Kenny Suykry <ksuyk001@ucr.edu>
2020-09-29 16:37:33 -0700

    Added div for gif and append in function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ async function getGiphy(evt) {
   evt.preventDefault();
   let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${evt.target.giph}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
   console.log("got", response);
+  $(".gifCreator").append(response);
   return response.data;
 }
 $("#search-form").on("submit", getGiphy)
